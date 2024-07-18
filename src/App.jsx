93774870c09eea2d6271a7c9efab7a6cdbb3490d
ahmed elshrkawy <ahmadelshrkawy232@gmail.com>
2024-07-18 12:54:01 +0300

    input

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import AddAttach from "./component/forms/fileInput/addAttach";
+import Listinput from "./component/forms/listInput/Listinput";
+import Error from "./models/Error";
+import Success from "./models/Success";
 
 function App() {
   return (
     <>
-      <AddAttach />
+      {/* <AddAttach /> */}
+      <Listinput />
     </>
   );
 }
