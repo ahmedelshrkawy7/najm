4516fad0ec4655267970078e8a6f2e6cb77a639e
ahmed elshrkawy <ahmadelshrkawy232@gmail.com>
2024-08-05 14:01:26 +0300

    updated

--- src/ProtectedRoutes.jsx
+++ src/ProtectedRoutes.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import TokenContext from "./store/TokenContext";
-import Login from "../Login";
+import Login from "./pages/admin/Login";
 
 const ProtectedRoutes = ({ children }) => {
   const { token } = useContext(TokenContext);
