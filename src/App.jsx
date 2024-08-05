aed5e96946338be3ba8d66c8810fa53233ecbe4a
mahmoud-adel-ahmed <mahmoudalghareeb253@gmail.com>
2024-08-05 12:14:03 +0300

    login added

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,18 @@ import Dashboard from "./Dashboard.jsx";
 import Success from "./models/Success.jsx";
 import Test from "./component/Reports/test.jsx";
 import AllAdmins from "./pages/admin/AllAdmins.jsx";
+import Login from "./pages/admin/Login.jsx";
 
 function App() {
   let { pathname } = useLocation();
-  console.log(pathname);
   return (
     <>
-      {pathname !== "/allAdmins" && <Navbar />}
+      {!/(admin)/gi.test(pathname) && <Navbar />}
       <Routes>
         <Route index element={<HomePage />} />
         <Route path="/ReportsPage" element={<ReportsPage />} />
         <Route path="/allAdmins" element={<AllAdmins />} />
+        <Route path="/loginAdmin" element={<Login />} />
         <Route path="/dash" element={<Dashboard />} />
         <Route path="/dash/:id" element={<Test />} />
       </Routes>
